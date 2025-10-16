ae97dcd921c7757c1434bc5afc8c8fd538632a8a
SingletonVD <singletonvd@mail.ru>
2025-10-17 02:19:48 +0300

    Add create note screen UI

--- app/src/main/java/com/singletonv/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/singletonv/notes/presentation/screens/notes/NotesScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
