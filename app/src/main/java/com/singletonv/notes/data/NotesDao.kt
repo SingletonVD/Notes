96ce318934ef82a1334405d227c6e7f49e032169
SingletonVD <singletonvd@mail.ru>
2025-10-18 06:07:20 +0300

    Add repository impl using db

--- app/src/main/java/com/singletonv/notes/data/NotesDao.kt
+++ app/src/main/java/com/singletonv/notes/data/NotesDao.kt
@@ -12,16 +12,19 @@ interface NotesDao {
     @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
     fun getAllNotes(): Flow<List<NoteDbModel>>
 
+    @Query("SELECT * FROM notes WHERE id = :noteId")
+    suspend fun getNote(noteId: Int): NoteDbModel
+
     @Query("SELECT * FROM notes " +
             "WHERE title LIKE '%' || :query || '%' OR " +
             "content LIKE '%' || :query || '%' " +
             "ORDER BY updatedAt DESC")
     fun searchNotes(query: String): Flow<List<NoteDbModel>>
 
-    @Query("DELETE FROM notes where id == :noteId")
+    @Query("DELETE FROM notes where id = :noteId")
     suspend fun deleteNote(noteId: Int)
 
-    @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id == :noteId")
+    @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id = :noteId")
     suspend fun switchPinnedStatus(noteId: Int)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
---
+++ app/src/main/java/com/singletonv/notes/data/NotesRepositoryImpl.kt
@@ -0,0 +1,62 @@
+package com.singletonv.notes.data
+
+import android.content.Context
+import com.singletonv.notes.domain.Note
+import com.singletonv.notes.domain.NotesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class NotesRepositoryImpl private constructor(context: Context) : NotesRepository {
+
+    private val notesDatabase = NotesDatabase.getInstance(context)
+    private val notesDao = notesDatabase.notesDao()
+
+    override suspend fun addNote(
+        title: String,
+        content: String,
+        isPinned: Boolean,
+        updatedAt: Long
+    ) {
+        val noteDbModel = NoteDbModel(0, title, content, updatedAt, isPinned)
+        notesDao.addNote(noteDbModel)
+    }
+
+    override suspend fun deleteNote(noteId: Int) {
+        notesDao.deleteNote(noteId)
+    }
+
+    override suspend fun editNote(note: Note) {
+        notesDao.addNote(note.toDbModel())
+    }
+
+    override fun getAllNotes(): Flow<List<Note>> {
+        return notesDao.getAllNotes().map { it.toEntities() }
+    }
+
+    override suspend fun getNote(noteId: Int): Note {
+        return notesDao.getNote(noteId).toEntity()
+    }
+
+    override fun searchNotes(query: String): Flow<List<Note>> {
+        return notesDao.searchNotes(query).map { it.toEntities() }
+    }
+
+    override suspend fun switchPinStatus(noteId: Int) {
+        notesDao.switchPinnedStatus(noteId)
+    }
+
+    companion object {
+        private var instance: NotesRepositoryImpl? = null
+        private val LOCK = Any()
+
+        fun getInstance(context: Context): NotesRepositoryImpl {
+
+            instance?.let { return it }
+
+            synchronized(LOCK) {
+                instance?.let { return it }
+                return NotesRepositoryImpl(context)
+            }
+        }
+    }
+}
