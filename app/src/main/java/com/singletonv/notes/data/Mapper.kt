e17436b2eb22b55cf4bdfcf4569b95a31f53e0e1
SingletonVD <singletonvd@mail.ru>
2025-10-18 05:44:26 +0300

    Add notes dao

---
+++ app/src/main/java/com/singletonv/notes/data/Mapper.kt
@@ -0,0 +1,11 @@
+package com.singletonv.notes.data
+
+import com.singletonv.notes.domain.Note
+
+fun Note.toDbModel(): NoteDbModel {
+    return NoteDbModel(id, title, content, updatedAt, isPinned)
+}
+
+fun NoteDbModel.toEntity(): Note {
+    return Note(id, title, content, updatedAt, isPinned)
+}
